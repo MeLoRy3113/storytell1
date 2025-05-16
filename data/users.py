2eb9fa70e12fee67e67336bafce31c9906f4f7af
MeLoRy3113 <149680888+MeLoRy3113@users.noreply.github.com>
2025-05-16 21:20:31 +0300

    Update users.py

--- data/users.py
+++ data/users.py
@@ -17,7 +17,8 @@ class User(SqlAlchemyBase, UserMixin):
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
 
-    news = orm.relationship("News", back_populates='user')
+    news = orm.relationship("News", back_populates="user")
+    ratings = orm.relationship("Rating", back_populates="user")
 
     def __repr__(self):
         return f'<User> {self.id} {self.name} {self.email}'
